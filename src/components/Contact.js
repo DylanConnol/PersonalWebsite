a0dfb1a30a9d29b2469fad006d0e3b19094f30d5
DylanConnol <109382069+DylanConnol@users.noreply.github.com>
2024-03-28 19:48:46 -0400

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+function Contact () {
+    return (
+        <div>
+            <subpage>
+            <subpageTitle>Contact Information</subpageTitle>
+            </subpage>
+        </div>
+
+    )
+}
+
+export default Contact;
