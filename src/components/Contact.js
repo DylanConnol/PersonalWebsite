e4c5774601278c4c0945e959900b7b50350d630f
DylanConnol <109382069+DylanConnol@users.noreply.github.com>
2024-03-30 11:30:43 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,23 @@ function Contact () {
     return (
         <div>
             <subpage>
-            <subpageTitle>Contact Information</subpageTitle>
+            <title>Contact Information</title>
+            <images>
+            <a href="mailto:djconnol@bu.edu">
+            <img src="gmail.png"></img>
+            <span>djconnol@bu.edu</span>
+            </a>
+            <a href="tel:617-320-0153">
+            <img src="phone.png"></img>
+            <span>617-320-0153</span>
+
+            
+            </a>
+
+            </images>
+
+            
+
             </subpage>
         </div>
 
